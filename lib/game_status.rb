027a10588395044d00af85ad746cb8883af72e7c
Isaac Villicana <villicana.i@gmail.com>
2018-03-26 04:57:16 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,8 +15,8 @@ WIN_COMBINATIONS = [
   [2,4,6],
 ]
 
-def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
+#def won?(board)
+#  WIN_COMBINATIONS.each do |win_combination|
     #win_index_1 = win_combination[0]
     #win_index_2 = win_combination[1]
     #win_index_3 = win_combination[2]
@@ -24,20 +24,38 @@ def won?(board)
     #position_2 = board[win_combination[1]]
     #position_3 = board[win_combination[2]]
 
-    win_x = win_combination.all? do |positionx|
-      board[positionx] == "X"
+#  win_x = win_combination.all? do |positionx|
+#      board[positionx] == "X"
+  #  end
+  #  win_o = win_combination.all? do |positiono|
+  #    board[positiono] == "O"
+  #  end
+  #  if win_x == true || win_o == true
+  #    return win_combination
+  #  end
+#  end
+#  if board.all? { |empty| empty == " " || empty.nil?}
+  #  false
+#  else
+#    false
+#  end
+#end
+
+def won?(board)
+   WIN_COMBINATIONS.each do |win_combination|
+     win_index_1 = win_combination[0]
+     win_index_2 = win_combination[1]
+     win_index_3 = win_combination[2]
+
+     position_1 = board[win_index_1]
+     position_2 = board[win_index_2]
+     position_3 = board[win_index_3]
+
+     if (position_1 == "X"  && position_2 == "X" && position_3 == "X") | (position_1 == "O" && position_2 == "O" && position_3 == "O")
+       return win_combination
+    else
+      false
     end
-    win_o = win_combination.all? do |positiono|
-      board[positiono] == "O"
-    end
-    if win_x == true || win_o == true
-      return win_combination
-    end
-  end
-  if board.all? { |empty| empty == " " || empty.nil?}
-    false
-  else
-    false
   end
 end
 
